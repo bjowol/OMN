6f7dcd51259d8257dda014940b81a76338f23eca
bjowol <bjornwol@hotmail.com>
2018-06-12 10:55:03 +0200

    enkelt script som installerer vanlige moduler for å jobbe med office365/powershell

---
+++ Install-Office365Modules.psm1
@@ -0,0 +1,23 @@
+﻿function Install-Office365modules()
+{
+    $installedModules = (Get-Module).Name
+    $InstallModules = ("365Tools","AzureAD","MSOnline")
+    foreach($module in $InstallModules)
+    {
+        if($installedModules -like $module)
+        {
+            Write-Host "$module Is Installed"
+            
+        }#slutt if
+        else
+        {
+            Install-Module $module
+            Update-Module $module
+            Write-Host "Installed module $module" -ForegroundColor Green
+        
+        }#slutt else
+
+    }#slutt foreach
+
+}#Slutt funksjon
+
